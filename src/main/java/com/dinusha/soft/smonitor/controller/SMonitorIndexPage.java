32cd98aee4d96adb251b865547e77b7975649012
jayas <24741358+djerror327@users.noreply.github.com>
2021-04-24 11:50:48 +0530

    merged tow projects

--- src/main/java/com/dinusha/soft/smonitor/controller/SMonitorIndexPage.java
+++ src/main/java/com/dinusha/soft/smonitor/controller/SMonitorIndexPage.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class IndexPage {
+public class SMonitorIndexPage {
 
     @GetMapping("/s-monitor")
     public String SMonitorHomePage() {
